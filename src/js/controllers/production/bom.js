e3ffbb88d453d74ff6f81ff2bb9edeb9c34497d0
Vinodevan Vasudevan <vinodevan.v@gmail.com>
2018-11-22 22:48:40 -0500

    Code changes..

--- src/js/controllers/production/bom.js
+++ src/js/controllers/production/bom.js
@@ -1,10 +1,26 @@
 erpApp.controller('bomCtrl', ['erpAppConfig', '$scope', 'commonFact', function(erpAppConfig, $scope, commonFact) {
     var actions = angular.extend(angular.copy(commonFact.defaultActions), {
-    	getRm: function(context, data, key, field) {
-            // context.form.fields['rmCode'] = angular.extend(context.form.fields['rmCode'], {
-            //     filter: {}
-            // });
-            // context.actions.makeOptionsFields(context.form.fields['rmCode']);
+        callBackEdit: function(context, key) {
+            var partNos = [];
+            for (var i in context.form.fields['partNo'].options) {
+                var val = context.form.fields['partNo'].options[i].id;
+                if(!context.actions.findObjectByKey(context.listViewDataMaster, context.form.fields['partNo'].id, val)){
+                    partNos.push(val);
+                }
+            }
+            if(partNos.length === 0 && !key){
+                partNos = 'nil';
+            }
+            else if(key){
+                partNos.push(context.data.partNo);
+            }
+            context.form.fields['partNo'].filter = {
+                id: partNos
+            };
+            context.actions.makeOptionsFields(context.form.fields['partNo']);
+        },
+        callBackAdd: function(context) {
+            context.actions.callBackEdit(context);
         }
     });
     $scope.context = erpAppConfig.modules.production.bom;
