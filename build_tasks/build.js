c40379cafa5a4ae856ad81b6550dbdb8c005c215
Vinodevan Vasudevan <vinodevan.v@gmail.com>
2019-07-02 23:00:58 -0400

    Code changes...

--- build_tasks/build.js
+++ build_tasks/build.js
@@ -11,23 +11,27 @@ module.exports = function(config, task, gulp) {
             return JSON.stringify(appConfig);
         };
 
-    gulp.task('clean', task.clean = function() {
+    gulp.task('clean', task.clean = () => {
         return del(['./dist']);
     });
 
-    gulp.task('build-index', task.buildIndex = function() {
+    gulp.task('clean-template', task.clean = () => {
+        return del(['./dist/js/template.js']);
+    });
+
+    gulp.task('build-index', task.buildIndex = () => {
         return gulp.src('./src/index.html').pipe(gulp.dest('./dist'));
     });
 
-    gulp.task('build-lib', task.buildLib = function() {
+    gulp.task('build-lib', task.buildLib = () => {
         return gulp.src(config.src.lib, { base: './node_modules' }).pipe(gulp.dest(config.dist.lib));
     });
 
-    gulp.task('build-assets', task.buildAssets = function() {
+    gulp.task('build-assets', task.buildAssets = () => {
         return gulp.src(config.src.assets + '/**/**.**').pipe(gulp.dest(config.dist.assets));
     });
 
-    gulp.task('build-template', task.buildTemplate = function() {
+    gulp.task('build-template', task.buildTemplate = () => {
         return gulp.src(config.src.template + '/**/**.html')
             .pipe(ngHtml2Js({
                 moduleName: "erpApp",
@@ -37,14 +41,14 @@ module.exports = function(config, task, gulp) {
             .pipe(gulp.dest(config.dist.js));
     });
 
-    gulp.task('build-js', task.buildJs = function() {
+    gulp.task('build-js', task.buildJs = () => {
         return gulp.src(config.src.defaultSrcJsFiles)
             .pipe(gp_concat('boot.js'))
             .pipe(replace('STATIC_CONFIG', applyAppConfig))
             .pipe(gulp.dest(config.dist.js));
     });
 
-    gulp.task('build-js-minify', task.buildJsMinify = function() {
+    gulp.task('build-js-minify', task.buildJsMinify = () => {
         return gulp.src(config.src.defaultSrcJsFiles)
             .pipe(gp_concat('boot.js'))
             .pipe(replace('STATIC_CONFIG', applyAppConfig))
@@ -52,21 +56,11 @@ module.exports = function(config, task, gulp) {
             .pipe(gulp.dest(config.dist.js));
     });
 
-    gulp.task('build', ['clean', 'build-template'], task.build = function() {
-        task.buildIndex();
-        task.buildLib();
-        task.buildAssets();
-        task.buildJs();
-    });
+    gulp.task('build', gulp.series('clean', 'build-index', 'build-template', 'build-lib', 'build-assets', 'build-js', 'clean-template'));
 
-    gulp.task('build-minify', ['clean', 'build-template'], task.buildMinify = function() {
-        task.buildIndex();
-        task.buildLib();
-        task.buildAssets();
-        task.buildJsMinify();
-    });
+    gulp.task('build-minify', gulp.series('clean', 'build-index', 'build-template', 'build-lib', 'build-assets', 'build-js-minify', 'clean-template'));
 
-    gulp.task('build-project', task.buildProject = function() {
+    gulp.task('build-project', task.buildProject = () => {
         const stat = require('fs').statSync;
         const AdmZip = require('adm-zip');
 
