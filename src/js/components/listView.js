20a76f8e8843d27bc5f0eece68d94cc0479410cc
Vinodevan Vasudevan <vinodevan.v@gmail.com>
2020-07-05 21:32:59 -0400

    Release changes...

--- src/js/components/listView.js
+++
@@ -1,7 +0,0 @@
-erpConfig.moduleFiles.startFrom = function() {
-    return function(input, start) {
-        start = +start; //parse to int
-        return input && input.slice(start) || false;
-    }
-};
-erpApp.filter('startFrom', erpConfig.moduleFiles.startFrom);
