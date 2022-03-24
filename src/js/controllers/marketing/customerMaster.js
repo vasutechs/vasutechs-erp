aabe7f3e6b0b807150222147149c6ec046428dff
vinodevanv84 <vinodevan.v@gmail.com>
2022-03-24 12:53:00 -0400

    Updated code.

--- src/js/controllers/marketing/customerMaster.js
+++ src/js/controllers/marketing/customerMaster.js
@@ -2,16 +2,16 @@ erpConfig.moduleFiles.customerMaster = function(context) {
 	 var mappingField = null;
     return {
 		callBackAdd: function () {
-            mappingField = angular.copy(context.controller.form.mapping.fields);
-            context.controller.form.mapping.fields = [];
-			context.controller.methods.updateFieldMapping();
+            // mappingField = angular.copy(context.controller.form.mapping.fields);
+            // context.controller.form.mapping.fields = [];
+			// context.controller.methods.updateFieldMapping(mappingField);
         },
 		callbackList(){
-			context.controller.form.mapping.fields = mappingField;
+			//context.controller.form.mapping.fields = mappingField;
 		},
-        updateFieldMapping: function () {
+        updateFieldMapping: function (mappingField) {
            for (var i in context.controller.data.mapping) {
-                context.controller.form.mapping.fields[i] = angular.copy(mappingField);
+                context.controller.form.mapping.fields[i] = mappingField;
                 context.commonFact.getOperationFromFlow(context.controller.form.mapping.fields[i]['operationFrom'], {
                     partNo: context.controller.data.mapping[i].id
                 });
