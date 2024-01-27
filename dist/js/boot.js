8bd7b91ab5a2d5fd805aa97e9d39bb4aded70031
VinodevanVasudevan <vinodevan.v@gmail.com>
2024-01-27 13:11:52 -0500

    Code changed

--- dist/js/boot.js
+++ dist/js/boot.js
@@ -1683,6 +1683,66 @@ erpConfig.moduleFiles.login = function(context) {
         }
     };
 };
+erpConfig.moduleFiles.settings = function(context) {
+    return {
+        callBackList: function() {
+            if(context.controller.form.mapping){
+                var moduleField = context.controller.form.mapping.fields['module'];
+                moduleField.options = {};
+                moduleField.allOptions = {};
+                context.controller.methods.makeModuleOptions(context.erpAppConfig.modules.controllers, moduleField);
+            }
+            if (context.controller.lastData === undefined) {
+                context.commonFact.add();
+            } else {
+                context.commonFact.edit(context.controller.lastData.id);
+            }
+        },
+        callBackEdit: function(){
+            var custumOption = {
+                userType: 'all',
+                optionName: 'All',
+                optionId: 'all'
+            };
+            if(context.controller.form.mapping) { 
+                context.controller.form.mapping.fields['restrictUser'].options['all'] = custumOption;
+            }
+        },
+        makeModuleOptions: function(modules, field, parentModule) {
+            for (var i in modules) {
+                var module = angular.copy(modules[i]);
+                var optionIdVal = parentModule && parentModule.id + '.' + module.id || module.id;
+                var optionNameVal = parentModule && '-- ' + module.title || module.title;
+                if (!module.disable) {
+                    field.allOptions[optionIdVal] = module;
+                    field.allOptions[optionIdVal]['optionName'] = optionNameVal;
+                    field.allOptions[optionIdVal]['optionId'] = optionIdVal;
+                    field.options[optionIdVal] = field.allOptions[optionIdVal];
+
+                    if (!module.page) {
+                        context.controller.methods.makeModuleOptions(context.commonFact.showSubModule(modules[i]), field, modules[i]);
+                    }
+                }
+            }
+        }
+    };
+};
+erpConfig.moduleFiles.users = function(context) {
+    return {
+        callBackAdd: function() {
+            var adminOption = {
+                userType: 'ADMIN',
+                desc: 'ADMIN',
+                optionName: 'ADMIN',
+                optionId: 'ADMIN'
+            };
+            context.controller.form.fields['userType'].options['ADMIN'] = adminOption;
+        },
+        callBackEdit: function() {
+            context.controller.methods.callBackAdd();
+        }
+    };
+};
 erpConfig.moduleFiles.customerPaymentInvoice = function (context) {
     var orgField = null;
     return {
@@ -1936,66 +1996,6 @@ erpConfig.moduleFiles.suppilerPayment = function(context) {
             if (context.controller.data.balanceAmount < 0) {
                 context.controller.data.balanceAmount = 0;
             }
-        }
-    };
-};
-erpConfig.moduleFiles.settings = function(context) {
-    return {
-        callBackList: function() {
-            if(context.controller.form.mapping){
-                var moduleField = context.controller.form.mapping.fields['module'];
-                moduleField.options = {};
-                moduleField.allOptions = {};
-                context.controller.methods.makeModuleOptions(context.erpAppConfig.modules.controllers, moduleField);
-            }
-            if (context.controller.lastData === undefined) {
-                context.commonFact.add();
-            } else {
-                context.commonFact.edit(context.controller.lastData.id);
-            }
-        },
-        callBackEdit: function(){
-            var custumOption = {
-                userType: 'all',
-                optionName: 'All',
-                optionId: 'all'
-            };
-            if(context.controller.form.mapping) { 
-                context.controller.form.mapping.fields['restrictUser'].options['all'] = custumOption;
-            }
-        },
-        makeModuleOptions: function(modules, field, parentModule) {
-            for (var i in modules) {
-                var module = angular.copy(modules[i]);
-                var optionIdVal = parentModule && parentModule.id + '.' + module.id || module.id;
-                var optionNameVal = parentModule && '-- ' + module.title || module.title;
-                if (!module.disable) {
-                    field.allOptions[optionIdVal] = module;
-                    field.allOptions[optionIdVal]['optionName'] = optionNameVal;
-                    field.allOptions[optionIdVal]['optionId'] = optionIdVal;
-                    field.options[optionIdVal] = field.allOptions[optionIdVal];
-
-                    if (!module.page) {
-                        context.controller.methods.makeModuleOptions(context.commonFact.showSubModule(modules[i]), field, modules[i]);
-                    }
-                }
-            }
-        }
-    };
-};
-erpConfig.moduleFiles.users = function(context) {
-    return {
-        callBackAdd: function() {
-            var adminOption = {
-                userType: 'ADMIN',
-                desc: 'ADMIN',
-                optionName: 'ADMIN',
-                optionId: 'ADMIN'
-            };
-            context.controller.form.fields['userType'].options['ADMIN'] = adminOption;
-        },
-        callBackEdit: function() {
-            context.controller.methods.callBackAdd();
         }
     };
 };
